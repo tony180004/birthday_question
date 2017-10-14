81a15a66eb55ba50ccee5dd6788cfa8433965a1c
tony180004 <b03901105@ntu.edu.tw>
2017-10-14 22:55:48 +0800

    Demo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-birthday_q_a = 'birth.csv'
+birthday_q_a = 'birth2.csv'
 questions = pd.read_csv(birthday_q_a, usecols=[0, 1, 2]).values
 i = [0]
 which = random.sample(range(questions.shape[0]), questions.shape[0])
